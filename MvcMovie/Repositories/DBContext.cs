251fa2a706d58e8bbb3627a84f9fd04bfe8350e9
a2314178a <a2314178a@gmail.com>
2019-12-11 18:01:15 +0800

    login基本防護完成 新增、刪除使用者完成 建構更新使用者

--- MvcMovie/Repositories/DBContext.cs
+++ MvcMovie/Repositories/DBContext.cs
@@ -8,8 +8,8 @@ namespace MvcMovie.Repositories
     {
         public DBContext(DbContextOptions options) : base(options){ }
         public DbSet<Student> Students { get; set; }       //query use
-        public DbSet<Teacher> Teachers { get; set; } 
-         public DbSet<Subject> Subjects { get; set; } 
+        public DbSet<User> Users { get; set; } 
+        public DbSet<Subject> Subjects { get; set; } 
         /*protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Student>().ToTable<Student>("students");
--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -69,7 +69,7 @@ namespace MvcMovie
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Account}/{action=Index}/{id?}");
             });
 
             //dbContext.Database.EnsureCreated(); //建立資料庫
